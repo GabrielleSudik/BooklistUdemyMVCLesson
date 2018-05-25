cd3dea035798e37016c4d8a1bf09516856f86d2d
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-05-25 14:51:23 -0400

    first commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BooklistUdemyMVCLesson.Models;
+
+namespace BooklistUdemyMVCLesson.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Books.ToList());
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooklistUdemyMVCLesson.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
